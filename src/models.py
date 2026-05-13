ddefe92e517e128c2806f17caa565b31a2eaad20
mono <hossaus62@gmail.com>
2026-05-13 22:48:05 +0330

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,19 +1,8 @@
 from dataclasses import dataclass
-from typing import Optional, List
 
 
 @dataclass
 class Video:
     title: str
     video_id: str
-    webpage_url: str
-    direct_url: Optional[str] = None
-    duration: Optional[int] = None
-    uploader: Optional[str] = None
-
-
-@dataclass
-class DownloadResult:
-    source_url: str
-    title: str
-    videos: List[Video]
+    url: str
